d5791e6d5488ba93af4c3223131db3abe249d1b3
rory <rphelan@softwareinstitute.com>
2021-10-12 14:02:43 +0100

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("I hope this works");
+        System.out.println("I hope this works");
+        System.out.println("I hope this works");
+        System.out.println("I hope this works");
+        System.out.println("I hope this works");
     }
 }
